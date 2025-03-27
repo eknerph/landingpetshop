a438c283ce46832d1f9be3ee73cd456874d17044
eknerph <eknerph@gmail.com>
2025-03-26 21:18:11 -0300

    feat: Adicionado 2º complemento do javascript

--- script.js
+++ script.js
@@ -59,3 +59,12 @@ window.addEventListener("click", (event) => {
     }
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
